ad97e42da7f3c7ae733af36d5e32bb594afdb8fc
Sandra Larsson <sandral@mg.lan>
2020-05-19 11:45:37 +0200

    first commit

--- uppgift-blog-del3/controllers/postController.js
+++ uppgift-blog-del3/controllers/postController.js
@@ -2,12 +2,12 @@
 const Post = require("./../models/postModel");
 
 // Hämta alla inlägg - READ
-exports.getPosts = async function(requestuest, responseponse) {
+exports.getPosts = async function(request, response) {
  
     const allPosts = await Post.find();
     console.log(allPosts);
     // TODO: Felhantering
-    responseponse.render("posts/index", { posts: allPosts});
+    response.render("posts/index", { posts: allPosts});
 
  }
 
@@ -27,35 +27,35 @@ exports.createPost = async function(request, response) {
     await Post.create(newPost);
     // TODO: Felhantering 
 
-    responseponse.redirect("/posts");
+    response.redirect("/posts");
 
  }
 
  // Hämta specifikt inlägg - READ
-exports.getPost = async function(requestuest, responseponse) {
+exports.getPost = async function(request, response) {
 
-    const thePost = await Post.findById(requestuest.params.id);
+    const thePost = await Post.findById(request.params.id);
     //console.log(post);
     // TODO: Felhantering 
-    responseponse.render("posts/show", {post: thePost});
+    response.render("posts/show", {post: thePost});
 }
 
 // Hämta specifikt inlägg för uppdatera formulär - READ
-exports.editFormPost = async function(requestuest, responseponse) {
+exports.editFormPost = async function(request, response) {
 
-    const thePost = await Post.findById(requestuest.params.id);
+    const thePost = await Post.findById(request.params.id);
     console.log(thePost);
     // TODO: Felhantering 
 
-    responseponse.render("posts/edit", {post: thePost});
+    response.render("posts/edit", {post: thePost});
 }
 
 // Uppdater specifiktt inlägg - UPDATE
-exports.updatePost =  async function(requestuest, responseponse) {
+exports.updatePost =  async function(request, response) {
 
-    const title = requestuest.body.title;
-    const image = requestuest.body.image;
-    const body = requestuest.body.body;
+    const title = request.body.title;
+    const image = request.body.image;
+    const body = request.body.body;
 
     const newPost = {
         title: title,
@@ -63,16 +63,16 @@ exports.updatePost =  async function(requestuest, responseponse) {
         body: body
     }
 
-    await Post.findByIdAndUpdate(requestuest.params.id, newPost);
+    await Post.findByIdAndUpdate(request.params.id, newPost);
     // TODO: Felhantering
-    responseponse.redirect("/posts/" + requestuest.params.id);
+    response.redirect("/posts/" + request.params.id);
 }
 
 // Ta bort specifikt inlägg - DELETE
-  exports.deletePost = async function(requestuest, responseponse) {
+  exports.deletePost = async function(request, response) {
 
-    await Post.findByIdAndDelete(requestuest.params.id);
+    await Post.findByIdAndDelete(request.params.id);
     // TODO: Felhantering
 
-    responseponse.redirect("/posts");
+    response.redirect("/posts");
 }
